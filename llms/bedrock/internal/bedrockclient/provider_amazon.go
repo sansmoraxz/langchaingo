8283893197d033e6b883372cf3c8f712bc1ae1a3
sansmoraxz <31374920+sansmoraxz@users.noreply.github.com>
2024-03-12 01:26:05 +0530

    amz

--- llms/bedrock/internal/bedrockclient/provider_amazon.go
+++ llms/bedrock/internal/bedrockclient/provider_amazon.go
@@ -10,38 +10,50 @@ import (
 	"github.com/tmc/langchaingo/llms"
 )
 
-
-
-const (
-	AmazonCompletionReasonFinish = "FINISH"
-	AmazonCompletionReasonMaxTokens = "LENGTH"
-	AmazonCompletionReasonContentFiltered = "CONTENT_FILTERED"
-)
-
-// https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-titan-text.html
+// Ref: https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-titan-text.html
 
 
 type amazonTextGenerationConfigInput struct {
+	// The maximum number of tokens to generate per result. Optional, default = 512
 	MaxTokens int `json:"maxTokenCount,omitempty"`
+	// Use a lower value to ignore less probable options and decrease the diversity of responses. Optional, default = 1
 	TopP float64 `json:"topP,omitempty"`
+	// Use a lower value to decrease randomness in responses. Optional, default = 0.0
 	Temperature float64 `json:"temperature,omitempty"`
+	// Specify a character sequence to indicate where the model should stop.
+	// Currently only supports: ["|", "User:"]
 	StopSequences []string `json:"stopSequences,omitempty"`
 }
 
 type amazonTextGenerationInput struct {
+	// The text which the model is requested to continue.
 	InputText string `json:"inputText"`
+	// The configuration for the text generation
 	TextGenerationConfig amazonTextGenerationConfigInput `json:"textGenerationConfig"`
 }
 
 
 type amazonTextGenerationOutput struct {
+	// The number of tokens in the prompt
 	InputTextTokenCount int `json:"inputTextTokenCount"`
+	// The results of the request
 	Results []struct {
+		// The number of tokens in the response
 		TokenCount int `json:"tokenCount"`
+		// The generated text
 		OutputText string `json:"outputText"`
+		// The reason for the completion of the generation
+		// One of: FINISH, LENGTH, CONTENT_FILTERED
 		CompletionReason string `json:"completionReason"`
 	} `json:"results"`
 }
+
+// Finish reason for the completion of the generation for Amazon Models
+const (
+	AmazonCompletionReasonFinish = "FINISH"
+	AmazonCompletionReasonMaxTokens = "LENGTH"
+	AmazonCompletionReasonContentFiltered = "CONTENT_FILTERED"
+)
 
 func createAmazonCompletion(ctx context.Context,
 	client *bedrockruntime.Client,
