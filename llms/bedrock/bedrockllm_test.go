70fc1962bfe6c5defb4e096d8462b3bc66a50bb5
Souyama <souyamadebnath@gmail.com>
2024-03-12 09:56:06 +0000

    u

--- llms/bedrock/bedrockllm_test.go
+++ llms/bedrock/bedrockllm_test.go
@@ -2,7 +2,6 @@ package bedrock_test
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -11,37 +10,6 @@ import (
 	"github.com/tmc/langchaingo/llms/bedrock"
 	"github.com/tmc/langchaingo/schema"
 )
-
-var msgs []llms.MessageContent = []llms.MessageContent{
-	{
-		Role: schema.ChatMessageTypeSystem,
-		Parts: []llms.ContentPart{
-			llms.TextPart("You know all about AI."),
-		},
-	},
-	{
-		Role: schema.ChatMessageTypeHuman,
-		Parts: []llms.ContentPart{
-			llms.TextPart("Explain AI in 10 words or less."),
-		},
-	},
-}
-
-// All the test models.
-var models = []string{
-	bedrock.ModelAi21J2MidV1,
-	bedrock.ModelAi21J2UltraV1,
-	bedrock.ModelAmazonTitanTextLiteV1,
-	bedrock.ModelAmazonTitanTextExpressV1,
-	bedrock.ModelAnthropicClaude3Sonnet20240229V10,
-	bedrock.ModelAnthropicClaudeV21,
-	bedrock.ModelAnthropicClaudeV2,
-	bedrock.ModelAnthropicClaudeInstantV1,
-	bedrock.ModelCohereCommandTextV14,
-	bedrock.ModelCohereCommandLightTextV14,
-	bedrock.ModelMetaLlama213bChatV1,
-	bedrock.ModelMetaLlama270bChatV1,
-}
 
 func setUpTest() (*bedrockruntime.Client, error) {
 	cfg, err := config.LoadDefaultConfig(context.Background())
@@ -64,21 +32,52 @@ func TestAmazonOutput(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	msgs := []llms.MessageContent{
+		{
+			Role: schema.ChatMessageTypeSystem,
+			Parts: []llms.ContentPart{
+				llms.TextPart("You know all about AI."),
+			},
+		},
+		{
+			Role: schema.ChatMessageTypeHuman,
+			Parts: []llms.ContentPart{
+				llms.TextPart("Explain AI in 10 words or less."),
+			},
+		},
+	}
+
+	// All the test models.
+	models := []string{
+		bedrock.ModelAi21J2MidV1,
+		bedrock.ModelAi21J2UltraV1,
+		bedrock.ModelAmazonTitanTextLiteV1,
+		bedrock.ModelAmazonTitanTextExpressV1,
+		bedrock.ModelAnthropicClaude3Sonnet20240229V10,
+		bedrock.ModelAnthropicClaudeV21,
+		bedrock.ModelAnthropicClaudeV2,
+		bedrock.ModelAnthropicClaudeInstantV1,
+		bedrock.ModelCohereCommandTextV14,
+		bedrock.ModelCohereCommandLightTextV14,
+		bedrock.ModelMetaLlama213bChatV1,
+		bedrock.ModelMetaLlama270bChatV1,
+	}
+
 	ctx := context.Background()
 
 	for _, model := range models {
-		fmt.Println("--------------------------------------------------")
-		fmt.Printf("Model: %s\n", model)
+		t.Log("\n--------------------------------------------------\n")
+		t.Logf("Model: %s\n", model)
 
 		resp, err := llm.GenerateContent(ctx, msgs, llms.WithModel(model), llms.WithMaxTokens(512))
 		if err != nil {
 			t.Fatal(err)
 		}
 		for i, choice := range resp.Choices {
-			fmt.Printf("Choice %d:-\n", i)
-			fmt.Printf("%s\n", choice.Content)
+			t.Logf("Choice %d:-\n", i)
+			t.Logf("%s\n", choice.Content)
 		}
 
-		fmt.Println("--------------------------------------------------")
+		t.Log("\n--------------------------------------------------\n")
 	}
 }
